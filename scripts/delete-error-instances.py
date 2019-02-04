1450c703212bbf8343b1e0da69693f0765011daa
Tristan Cacqueray <tdecacqu@redhat.com>
2019-02-04 08:37:44 +0000

    Update delete-error-instances script to use openstacksdk
    Change-Id: I7bd90bc106574b9e9056e0dc35b63f3039089b79

--- scripts/delete-error-instances.py
+++ scripts/delete-error-instances.py
@@ -1,21 +1,12 @@
 #!/bin/env python3
 
-import shade
+import openstack
 import time
-cloud = shade.openstack_cloud()
-servers = cloud.list_servers()
-print("%d servers" % len(servers))
-
-# TODO: clean fip, ports, volumes, ...
-
-for server in filter(lambda x: x['status'] == 'ERROR', servers):
-  print("Deleting %s" % server['id'])
-  try:
-      if not cloud.delete_server(server['id']):
-          print("delete failed")
-  except Exception as e:
-      print('couldnt delete...', e)
-  time.sleep(0.5)
-
-servers = cloud.list_servers()
-print("%d servers remaining" % len(servers))
+cloud = openstack.connect()
+for server in cloud.compute.servers():
+    if server['status'] == 'ERROR':
+        try:
+            print("Deleting %s" % server['id'])
+            cloud.compute.delete_server(server['id'], force=True)
+        except Exception as e:
+            print('couldnt delete...', e)
