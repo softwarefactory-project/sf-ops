1c48051d1c47f427b6523f381e7bde8624194dbe
Javier Pena <jpena@redhat.com>
2019-06-04 17:10:35 +0200

    Allow option to force-delete a node to zk-helper.py
    In some cases, we have seen a node stuck in "in-use" state in nodepool,
    even when there is no Zuul job using it. For those cases, we want
    to use a --force-delete command-line option in zk-helper.py, so we
    can forcefully delete this node from Zookeeper.
    
    Change-Id: I4ecffa1ea13c0f5afc87a5edcd713f55ee2a0a36

--- scripts/zk-helper.py
+++ scripts/zk-helper.py
@@ -1,5 +1,7 @@
 #!/opt/rh/rh-python35/root/usr/bin/python
 
+import argparse
+import json
 import kazoo.client
 import sys
 import os.path
@@ -13,14 +15,12 @@ def print_root(root):
         print(os.path.join(root, child))
         print_root(os.path.join(root, child))
 
-import argparse
-
 p = argparse.ArgumentParser()
 p.add_argument("--list", action="store_true")
 p.add_argument("--deleting", action="store_true")
 p.add_argument("--requesting", action="store_true")
+p.add_argument("--force-delete", help="Force-delete this node id")
 args = p.parse_args()
-import json
 
 if args.list:
     print_root("/nodepool")
@@ -50,3 +50,17 @@ elif args.deleting:
             client.delete(node_path, recursive=True)
         else:
             print(node_path, node["state"])
+elif args.force_delete:
+    nodeid = args.force_delete
+    base_path = "/nodepool/nodes"
+    node_path = os.path.join(base_path, nodeid)
+    if not client.exists(node_path):
+        print("Node path does not exist")
+    else:
+        lock_path = os.path.join("/nodepool/nodes/lock", nodeid)
+
+        print("Force-deleting node %s" % nodeid)
+        client.delete(node_path, recursive=True)
+        if client.exists(lock_path):
+            print("Force-deleting lock path %s" % lock_path)
+            client.delete(lock_path, recursive=True)
