aaf26c3afdd22c5e793b99e76f8fa4eaff6e25f2
Tristan Cacqueray <tdecacqu@redhat.com>
2020-07-08 13:28:01 +0000

    Add zk-tools library to modify zookeeper state
    This change introduces a new zk-tools library to be used
    from the REPL to modify zookeeper state.
    
    Change-Id: I015e08260ed9afba0ff180433b18d578a04e3a94

---
+++ scripts/zk-tools.py
@@ -0,0 +1,94 @@
+#!/bin/env python3
+# Copyright 2020, Red Hat
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# A library to interact with zookeeper
+#
+# Examples:
+## Get client
+# zk = client("zookeeper")
+## Remove a single node
+# delete_zkpath(zk, "/nodepool/nodes/0000081444")
+## Get nodes of a providers:
+# runc = filter_by_provider("managed-runc-provider")(get_nodes(zk))
+## Get nodes path
+# print(get_paths(runc))
+## Delete nodes
+# list(map(delete_zknode(zk), get_paths(runc)))
+## Count deleting node that are older than 15 min
+# len(filter_by_min_age(60 * 15)(filter_by_state("deleting")(get_nodes(zk))))
+
+from pathlib import Path
+import time
+import json
+import kazoo.client
+
+def client(host):
+    client = kazoo.client.KazooClient(hosts=host)
+    client.start()
+    return client
+
+def decode_zknode(node_data):
+    try:
+        return json.loads(node_data[0])
+    except Exception as e:
+        print("%s: failed to decode: %s" % (e, node_data))
+        return None
+
+def delete_zkpath(client, zk_path):
+    zk_path_str = str(zk_path)
+    if client.exists(zk_path_str):
+        print("%s: deleting" % zk_path_str)
+        client.delete(zk_path_str, recursive=True)
+
+def delete_zknode(client):
+    def _func(node_path):
+        lock_path = Path("/nodepool/nodes/lock") / node_path.name
+        delete_zkpath(client, lock_path)
+        delete_zkpath(client, node_path)
+        return True
+    return _func
+
+def get_paths(tuple_list):
+    return list(map(lambda x: x[0], tuple_list))
+
+def get_nodes(client):
+    base = Path("/nodepool/nodes")
+    return list(filter(lambda x: x[1] is not None, [
+        (base / node, decode_zknode(client.get(str(base / node))))
+        for node in client.get_children(str(base))
+    ]))
+
+def filter_by_provider(name):
+    def _func(nodes):
+        return list(filter(lambda x: x[1]['provider'] == name, nodes))
+    return _func
+
+def filter_by_state(name):
+    def _func(nodes):
+        return list(filter(lambda x: x[1]['state'] == name, nodes))
+    return _func
+
+def filter_by_min_age(age):
+    """ age is in second """
+    def _func(nodes):
+        return list(filter(lambda x: time.time() - x[1]['state_time'] >= age, nodes))
+    return _func
+
+def order_by_age(tuple_list):
+    return list(sorted(tuple_list, key=lambda t: t[1]['state_time']))
+
+def format_state_time(tuple_list):
+    return ["%s: %s" % (node_path, time.ctime(zk_node['state_time']))
+            for (node_path, zk_node) in tuple_list]
