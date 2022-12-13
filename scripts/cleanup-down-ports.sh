20b08c8c190b39cc2acf9999c53710a219a7e9c6
Daniel Pawlik <dpawlik@redhat.com>
2022-12-13 08:42:15 +0100

    Add summary information about deleted and skipped ports
    The cleanup-down-ports.sh script is printing line by line information
    if port has been deleted or skipped. It would be good to get summary
    information.
    
    Change-Id: I1731d68c8576c89f674d3c5ab568178e08abb36e

--- scripts/cleanup-down-ports.sh
+++ scripts/cleanup-down-ports.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+skipped_counter=0
+deleted_counter=0
+
 DAY_AGO=$(date -d "$(date '+%Y-%m-%dT%H:%M:%S' -d '1 day ago')" +%s)
 for port in $(openstack port list | grep -i down | awk '{print $2}'); do
     PORT_DATE=$(date -d "$(openstack port show -c updated_at -f value $port)" +%s);
@@ -7,5 +10,10 @@ for port in $(openstack port list | grep -i down | awk '{print $2}'); do
     if [ "$DAY_AGO" -gt "$PORT_DATE" ]; then
         echo "Deleting port $port"
         openstack port delete $port;
+        deleted_counter=$((deleted_counder+1))
+    else
+        skipped_counter=$((skipped_counter+1))
     fi;
 done
+
+echo "The script have deleted: ${deleted_counter} and skip: ${skipped_counter} ports"
